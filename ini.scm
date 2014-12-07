ae9224e6b1cb5bb4d74ec252a2eceadbc9678a31
Chase Hoselle <choselle@yahoo.com>
2014-12-06 21:03:26 -0600

    Finishing up some things.

--- ini.scm
+++ ini.scm
@@ -23,7 +23,6 @@
     (if (eq? (car l) #f) #f (apply and (cdr l)))))
 ;or
 (define (or . l) (if (eq? (car l) #f) (apply or (cdr l)) (car l)))
-
 ;reverse
 (define (reverse l)
   (if (null? l) '()
@@ -82,13 +81,12 @@
         ((< n 0) #t)
         (else #f)))
 ;odd?
-(define (odd? n)
-  (if (eqv? n 0) #f
-      (even? (- n 1))))
+(define (odd? x) 
+  (cond ((= x 0) #t) ((= x 1) #f) 
+    (else (odd? (abs (- 2 x))))))
 ;even?
-(define (even? n)
-  (if (eqv? n 0) #t
-      (odd? (- n 1))))
+(define (even? x) 
+  (not (odd? x)))
 ;max
 (define (max . l)
   (if (> (car l) (car (cdr l))) (car l)
@@ -119,93 +117,58 @@
   (cond ((eqv? x y) #t)
     ((and (pair? x) (pair? y) (equal? (car x) (car y)) (equal? (cdr x) (cdr y))) #t)
     (else #f)))
-
 ;=
 (define (= . l)
   (if (null? l) 0
-      (cond 
-        ((zero? (- (car l) (car (cdr l)))))
-        (else #f))))
-;<
-(define (< . l)  
-  (if (null? l) 0
-      (cond
-        ((negative? (- (car l) (car (cdr l)))))
-        (else #f))))
+    (b= (car l) (apply = (cdr l)))))
+
 ;>
 (define (> . l)
   (if (null? l) 0
-      (cond
-        ((positive? (- (car l) (car (cdr l)))))
-        (else #f))))
-;<=
-(define (<= . l)
+    (b> (car l) (apply > (cdr l)))))
+
+;<
+(define (< . l)
   (if (null? l) 0
-      (cond
-        ((zero? (- (car l) (car (cdr l)))))
-        ((negative? (- (car l) (car (cdr l)))))
-        (else #f))))
+    (b< (car l) (apply < (cdr l)))))
+
 ;>=
 (define (>= . l)
   (if (null? l) 0
-      (cond
-        ((zero? (- (car l) (car (cdr l)))))
-        ((positive? (- (car l) (car (cdr l)))))
-        (else #f))))
+    (b>= (car l) (apply >= (cdr l)))))
 
+;<=
+(define (<= . l)
+  (if (null? l) 0
+    (b<= (car l) (apply <= (cdr l)))))
 ;list functions
 (define (caar l) (car (car l)))
-
 (define (cadr l) (car (cdr l)))
-
 (define (cdar l) (cdr (car l)))
-
 (define (cddr l) (cdr (cdr l)))
 
 (define (caaar l) (car (car (car l))))
-
 (define (caadr l) (car (car (cdr l))))
-
 (define (cadar l) (car (cdr (car l))))
-
 (define (cdaar l) (cdr (car (car l))))
-
 (define (caddr l) (car (cdr (cdr l))))
-
 (define (cdadr l) (cdr (car (cdr l))))
-
 (define (cddar l) (cdr (cdr (car l))))
-
 (define (cdddr l) (cdr (cdr (cdr l))))
 
 (define (caaadr l) (car (car (car (cdr l)))))
-
 (define (caadar l) (car (car (cdr (car l)))))
-
 (define (cadaar l) (car (cdr (car (car l)))))
-
 (define (cadadr l) (car (cdr (car (cdr l)))))
-
 (define (cdaadr l) (cdr (car (car (cdr l)))))
-
 (define (cdaddr l) (cdr (car (cdr (cdr l)))))
-
 (define (cddadr l) (cdr (cdr (car (cdr l)))))
-
 (define (cadddr l) (car (cdr (cdr (cdr l)))))
-
 (define (caaddr l) (car (car (cdr (cdr l)))))
-
 (define (cdddar l) (cdr (cdr (cdr (car l)))))
-
 (define (cddaar l) (cdr (cdr (car (car l)))))
-
 (define (cdaaar l) (cdr (car (car (car l)))))
-
 (define (cdadar l) (cdr (car (cdr (car l)))))
-
 (define (caddar l) (car (cdr (cdr (car l)))))
-
 (define (cddddr l) (cdr (cdr (cdr (cdr l)))))
-
 (define (caaaar l) (car (car (car (car l)))))
